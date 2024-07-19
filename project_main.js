a88fef30ee7ce11eab858f77bc1dd02f08bf5f0b
Daniel Chin <DanChin327@gmail.com>
2024-07-19 23:22:18 +0900

    finished styling

--- project_main.js
+++ project_main.js
@@ -1,10 +1,16 @@
+
 document.addEventListener('DOMContentLoaded', function() {
+
+  // set an ID from the HTML (One from the Moon Picture. The other is the entire dark css style.)
   const themeToggle = document.getElementById('theme-toggle');
   const darkStyle = document.getElementById('dark-style');
 
+
+// themeToggle -> comes from the const above.
   themeToggle.addEventListener('click', function(event) {
     event.preventDefault();
 
+    //the disabled comes from the HTML css dark style tag.
     if (darkStyle.disabled) {
       darkStyle.disabled = false;
       themeToggle.innerHTML = '<i class="fa-regular fa-sun"></i>';
